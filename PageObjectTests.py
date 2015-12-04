3191821045195d43973c03d28a801c9349508deb
Joseph N Conlin <conlinjn@ldschurch.org>
2015-12-04 15:50:43 -0700

    Get excel input working

--- PageObjectTests.py
+++ PageObjectTests.py
@@ -6,10 +6,9 @@ from TestBrowser import TestBrowser
 from HeaderPage import Header
 from TheatersPage import Theaters
 import TheaterDetailPage
-# from FileInput import ReadExcel
+from FileInput import ReadExcel
 
 import unittest
-from selenium.common.exceptions import NoSuchElementException
 from random import randint
 
 # Setup some common test variables
@@ -30,21 +29,20 @@ class HeaderTests(unittest.TestCase):
         self.header.do_search(_headerSearchTextNoSpaces)
         newPage = self.driver.current_url
         self.assertNotEqual(currentPage, newPage, "Searching did not navigate to a new page")
-        # self.assertIn(_headerSearchTextNoSpaces, newPage, "Search text not found in search URL string")
 
-    # def test_search_random_input_from_excel(self):
-    #     # Get a random row greater than 0 to avoid the header and get that search data from the default input file
-    #     # Within each row of data in the input file, [0] is the search string, [1] is the theater name, [2] is zip code
-    #     index = randint(1,6)
-    #     input = ReadExcel.get_sheet_values()
-    #
-    #     searchText = input[index][0]
-    #     expectedZip = str(input[index][2])
-    #     currentPage = self.driver.current_url
-    #     self.header.do_search(searchText)
-    #     newPage = self.driver.current_url
-    #     self.assertNotEqual(currentPage, newPage, "Searching did not navigate to a new page")
-    #     self.assertIn(expectedZip, self.driver.page_source, "Expected zip code not found in results page")
+    def test_search_random_input_from_excel(self):
+        # Get a random row greater than 0 to avoid the header and get that search data from the default input file
+        # [0] is the search string, [1] is the theater index, [2] is theater name, [3] is zip code
+        index = randint(1,6)
+        input = ReadExcel.get_sheet_values()
+
+        searchText = input[index][0]
+        expectedZip = str(input[index][3])
+        currentPage = self.driver.current_url
+        self.header.do_search(searchText)
+        newPage = self.driver.current_url
+        self.assertNotEqual(currentPage, newPage, "Searching did not navigate to a new page")
+        self.assertIn(expectedZip, self.driver.page_source, "Expected zip code not found in results page")
 
 
 class TheatersTests(unittest.TestCase):
@@ -62,29 +60,6 @@ class TheatersTests(unittest.TestCase):
         self.assertNotEqual(0, len(self.theaters.theatersList), "Did not create theaters list")
         self.assertNotEqual(0, len(self.theaters.theatersList[0]), "Did not get a valid list of theaters")
 
-    # def test_search_random_input_from_excel(self):
-    #     # Get a random row greater than 0 to avoid the header and get that search data from the default input file
-    #     # Within each row of data in the input file, [0] is the search string, [1] is the theater name, [2] is zip code
-    #     index = randint(1,6)
-    #     input = ReadExcel.get_sheet_values()
-    #
-    #     searchText = input[index][0]
-    #     theaterText = input[index][1]
-    #
-    #     if(_headerSearchText != searchText):
-    #         # Setup did a different search than we want - redo the search and update the variables
-    #         self.header = Header(self.driver)
-    #         self.header.do_search(searchText)
-    #         self.theaters = Theaters(self.driver)
-    #
-    #     currentPage = self.driver.current_url
-    #     self.theaters.click_theater(theaterText)
-    #     newPage = self.driver.current_url
-    #     theaterName = TheaterDetailPage.TheaterDetail(self.driver).theaterName
-    #     self.assertNotEqual(currentPage, newPage, "Selecting a theater did not navigate to a new page")
-    #     self.assertIn(theaterText.lower(), theaterName.lower(),
-    #                   "Did not end up on theater detail page for selected theater")
-
 
 class TheaterDetailTests(unittest.TestCase):
     def setUp(self):
@@ -93,7 +68,6 @@ class TheaterDetailTests(unittest.TestCase):
         self.header = Header(self.driver)
         self.header.do_search(_headerSearchText)
         self.theaters = Theaters(self.driver)
-        # self.theaters.click_theater(_theaterLinkText)
         self.theater = TheaterDetailPage.TheaterDetail(self.driver)
         self.theaterCalendar = TheaterDetailPage.TheaterCalendar(self.driver)
 
@@ -119,6 +93,27 @@ class TheaterDetailTests(unittest.TestCase):
         self.assertNotEqual(currentSelectDate+currentMovieList[0].movieShowTimeList[0],
                             newSelectDate+newMovieList[0].movieShowTimeList[0],
                             "Movie date and time from today matches movie date and time from tomorrow")
+
+    def test_search_random_input_from_excel(self):
+        # Get a random row greater than 0 to avoid the header and get that search data from the default input file
+        # [0] is the search string, [1] is the theater index, [2] is theater name, [3] is zip code
+        index = randint(1,6)
+        input = ReadExcel.get_sheet_values()
+
+        searchText = input[index][0]
+        theaterIndex = input[index][1]
+        theaterText = input[index][2]
+
+        if(_headerSearchText != searchText):
+            # Setup did a different search than we want - redo the search and update the variables
+            self.header = Header(self.driver)
+            self.header.do_search(searchText)
+            self.theaters = Theaters(self.driver)
+            self.theater = TheaterDetailPage.TheaterDetail(self.driver, theaterIndex)
+
+        theaterName = self.theater.theaterName
+        self.assertIn(theaterText.lower(), theaterName.lower(),
+                      "Did not end up on theater detail page for selected theater")
 
 
 if __name__ == '__main__':
